db2b59a5e2cc3ea0e28d248cc358db9dfe14b6eb
Andrea T <tostonar@gmail.com>
2019-10-08 16:57:32 -0400

    Done.

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ const leashDog = (dogName, dogBreed) => (`Leash ${dogName} the ${dogBreed}`);
 const walkToPark = (dogName, dogBreed) => {
   return `Walk to the park with ${dogName} the ${dogBreed}`;
 }
+
 const throwFrisbee = (dogName, dogBreed) => {
   return `Throw the frisbee for ${dogName} the ${dogBreed}`;
 }
+
 const walkHome = (dogName, dogBreed) => {
   return `Walk home with ${dogName} the ${dogBreed}`;
 }
+
 const  unleashDog = (dogName, dogBreed) => {
   return `Unleash ${dogName} the ${dogBreed}`;
 }
+
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
 function exerciseDog(dogName, dogBreed) {
   const successMessages = []
   routine.forEach(function(e) {successMessages.push(e(dogName, dogBreed))});
